619733454f8d2cf1cf27f67572d0ec7a74330a43
ChenYueyi <39213643+ChenYueyi@users.noreply.github.com>
2018-05-27 23:31:44 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {  ApplicationsComponent } from './applications.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ApplicationsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/applications.component.ts
@@ -0,0 +1,9 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'applications',
+    template: '<h2>Applications</h2>' 
+})
+export class ApplicationsComponent{
+}
